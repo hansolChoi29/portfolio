bca1e4b71f422d9c13cbfabee528fb92bec2c9bd
hansolChoi29 <ggksthf29@gmail.com>
2025-04-11 13:51:15 +0900

    feat:footer생성

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>@hansol_Choi</h1>
+
+      <div>
+        <p>전화번호 : </p>
+        <p>010-2915-2931</p>
+      </div>
+
+      <div>
+        <p>E-mail : </p>
+        <p>ggksthf29@gmail.com</p>
+      </div>
+
+      <div>
+        <p>Instagram : </p>
+        <Link
+          href="https://instagram.com/_hxnxol_"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-red-500 "
+        >
+          {/* underline */}
+          @_hxnxol_
+        </Link>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/components/IntroSlide.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntroSlide = () => {
+  return <div>IntroSlide</div>;
+};
+
+export default IntroSlide;
