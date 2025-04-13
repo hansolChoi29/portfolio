6e6ce792de874f934e7317f61d38222cea04edd5
hansolChoi29 <ggksthf29@gmail.com>
2025-04-13 13:34:15 +0900

    fix: dogo img의 가로세로 명시해줌

--- src/app/dogoPr/components/ModalContent.tsx
+++ src/app/dogoPr/components/ModalContent.tsx
@@ -496,7 +496,7 @@ export default function ModalContent({
           }}
           className="fixed sm:bottom-[50px] bottom-[50px] sm:right-[130px] right-[30px] z-[10000]  text-white sm:text-sm text-sm px-4 py-2  hover:animate-bounce transition"
         >
-          <img src="/icons/arrow.svg" className="w-12 h-12" />
+          <img src="/icons/arrow.svg" width={48} height={48} />
         </button>
       </motion.div>
     </AnimatePresence>
